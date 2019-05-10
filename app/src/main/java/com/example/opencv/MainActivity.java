2c0761c5c29fdea04a4b6b8ebe5e7b44e03ab621
Pelin Tuncel <41865921+pelodi@users.noreply.github.com>
2019-05-10 17:54:16 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/opencv/MainActivity.java
+++ app/src/main/java/com/example/opencv/MainActivity.java
@@ -1,15 +1,14 @@
 package com.example.opencv;
 
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     static {
@@ -22,15 +21,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button cameraInit = (Button) findViewById(R.id.cameraInit);
-        cameraInit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),EdgeDetection.class);
-                startActivity(i);
+        cameraInit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(final int requestCode,
+                                           @NonNull final String[] permissions,
+                                           @NonNull final int[] grantResults) {
+        if (PermissionsUtil.isRequestCodeForCamera(requestCode)) {
+            if (PermissionsUtil.isCameraGranted(this)) {
+                // permission was granted, yay! Do the
+                // contacts-related task you need to do.
+                startActivity(EdgeDetection.newIntent(this));
+            } else {
+                // permission denied, boo! Disable the
+                // functionality that depends on this permission.
+                Toast.makeText(this, R.string.camera_permission_denied, Toast.LENGTH_LONG).show();
             }
-        });
+        }
+    }
+
+    @Override
+    public void onClick(final View v) {
+        if (PermissionsUtil.isCameraGranted(this)) {
+            startActivity(EdgeDetection.newIntent(this));
+        } else {
+            PermissionsUtil.requestCaneraPermission(this);
+        }
     }
 
     public native String stringFromJNI();
-
 }
